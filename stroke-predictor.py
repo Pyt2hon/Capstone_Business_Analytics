da6ac1fbfdcf4d5802c13b9e560c19a0a9dad84b
Pyt2hon <101404002+Pyt2hon@users.noreply.github.com>
2022-05-20 19:26:33 +0200

    Update stroke-predictor.py

--- stroke-predictor.py
+++ stroke-predictor.py
@@ -1,6 +1,7 @@
-# Imports all necessary libraries
+# Import all necessary libraries
 import pandas as pd
 import streamlit as st
+import pickle
 import numpy as np
 import matplotlib as plt
 
@@ -11,31 +12,31 @@ import matplotlib as plt
 #st.pyplot(bins = 10)
 
 
-# Function that loads in the healthcare dataset
+# Implement Function that loads in the healthcare dataset
 @st.cache()
 def load_data():
     data = pd.read_csv("healthcare-dataset-stroke-data.csv")
     return data.dropna()
 
 
-# Saving it in the variable Stroke_data
+# Save it in the variable Stroke_data
 Stroke_data = load_data()
 
 
-# Sets the website title to "Stroke Risk Predictor" using streamlit
+# Set the website title to "Stroke Risk Predictor" using streamlit
 st.title("Stroke Risk Predictor")
 
-# Sets the header to "Customizable Predictions"
+# Set the header to "Customizable Predictions"
 st.header("Customizable Predictions")
 
-# Creates 10 objects with the same widths, where the individual variable-names indicate their position
+# Create 10 objects with the same widths, where the individual variable-names indicate their position
 row1_col1, row1_col2 = st.columns([1, 1])
 row2_col1, row2_col2 = st.columns([1, 1])
 row3_col1, row3_col2 = st.columns([1, 1])
 row4_col1, row4_col2 = st.columns([1, 1])
 row5_col1, row5_col2 = st.columns([1, 1])
 
-# Sets individual titles for each variable
+# Set individual titles for each variable
 row1_col1.write("Gender")
 row1_col2.write("Age")
 row2_col1.write("Hypertension")
@@ -47,66 +48,66 @@ row4_col2.write("Glucose level")
 row5_col1.write("BMI")
 row5_col2.write("Smoking status")
 
-# Displays a radio button widget for the user to enter the gender
+# Display a radio button widget for the user to enter the gender
 Gender = row1_col1.radio(
     "Enter gender:",
     options=['Male', 'Female', 'Other'], )
 row1_col1.write(Gender)
 
-# Displays a slider widget for the user to enter integer values from 0 to 120
+# Display a slider widget for the user to enter integer values from 0 to 120
 Age = row1_col2.slider("Enter age:",
                        min_value=0,
                        max_value=120,
-                       value=60)  # Sets a default value of 60
-row1_col2.write(Age)  # Shows the user the entered input
+                       value=60)  # Set a default value of 60
+row1_col2.write(Age)  # Show the user the entered input
 
-# Displays a radio widget for the user to enter the hypertension state
+# Display a radio widget for the user to enter the hypertension state
 Hypertension = row2_col1.radio(
     "Do you have hypertension?",
     options=['Yes', 'No'])
-row2_col1.write(Hypertension)  # Shows the user the entered input
+row2_col1.write(Hypertension)  # Show the user the entered input
 
-# Displays a radio widget for the user to enter the heart disease state
+# Display a radio widget for the user to enter the heart disease state
 Heart_disease = row2_col2.radio(
     "Have you ever suffered from a heart disease?",
     options=['Yes', 'No'])
-row2_col2.write(Heart_disease)  # Shows the user the entered input
+row2_col2.write(Heart_disease)  # Show the user the entered input
 
-# Displays a radio widget for the user to enter the marriage status
+# Display a radio widget for the user to enter the marriage status
 Marriage_status = row3_col1.radio(
     "Were you ever married?",
     options=['Yes', 'No'])
-row3_col1.write(Marriage_status)  # Shows the user the entered input
+row3_col1.write(Marriage_status)  # Show the user the entered input
 
-# Displays a select widget for the user to enter the latest type of work
+# Display a select widget for the user to enter the latest type of work
 Work_type = row3_col2.selectbox(
     "What is your latest type of work?",
     options=['Private Sector', 'Self-employed', 'Government Job', 'Child/ Student', 'Never worked'])
 row3_col2.write(Work_type)  # Shows the user the entered input
 
-# Displays a radio widget for the user to enter the residence type
+# Display a radio widget for the user to enter the residence type
 Residence_type = row4_col1.radio("Enter residence type:",
                                  options=['Urban', 'Rural'])
-row4_col1.write(Residence_type)  # Shows the user the entered input
+row4_col1.write(Residence_type)  # Show the user the entered input
 
-# Displays a slider widget for the user to enter integer values from 0 to 300
+# Display a slider widget for the user to enter integer values from 0 to 300
 Glucose_level = row4_col2.slider("Enter glucose level:",
                                  min_value=0,
                                  max_value=300,
-                                 value=150)  # Sets a default value of 150
-row4_col2.write(Glucose_level)  # Shows the user the entered input
+                                 value=150)  # Set a default value of 150
+row4_col2.write(Glucose_level)  # Show the user the entered input
 
-# Displays a slider widget for the user to enter integer values from 10.3 to 97.6
+# Display a slider widget for the user to enter integer values from 10.3 to 97.6
 BMI = row5_col1.slider("Enter BMI:",
-                       min_value=Stroke_data["bmi"].min(),  # Sets the min value from the dataset as the min_value
-                       max_value=Stroke_data["bmi"].max(),  # Sets the max value from the dataset as the max_value
-                       value=30.0)  # Sets a default value of 150
-row5_col1.write(BMI)  # Shows the user the entered input
+                       min_value=Stroke_data["bmi"].min(),  # Set the min value from the dataset as the min_value
+                       max_value=Stroke_data["bmi"].max(),  # Set the max value from the dataset as the max_value
+                       value=30.0)  # Set a default value of 150
+row5_col1.write(BMI)  # Show the user the entered input
 
-# Displays a radio widget for the user to enter the smoking status
+# Display a radio widget for the user to enter the smoking status
 Smoking_status = row5_col2.radio("Enter your smoking status:",
                                   options=['Never smoked', 'Formerly smoked', 'Smokes', 'Unknown'])
-row5_col2.write(Smoking_status)  # Shows the user the entered input
+row5_col2.write(Smoking_status)  # Show the user the entered input
 
 
 
@@ -116,7 +117,6 @@ st.write(f'Your probability for a stroke is:')
 if st.checkbox("Show filtered data", False):
     st.subheader("Raw Data")
     st.write(Stroke_data)
-
 
 
 
@@ -136,23 +136,22 @@ if st.checkbox("Show filtered data", False):
 
 uploaded_data = st.file_uploader("Choose a file with Customer Data for predicting Stroke")
 
+
+@st.cache(allow_output_mutation=True)
+def load_model():
+    filename = "Part1_finished.ipynb"
+    loaded_model = pickle.load(open(filename, "rb"))
+    return loaded_model
+model = load_model()
+
 if uploaded_data is not None:
     new_customers = pd.read_csv(uploaded_data)
     st.write(new_customers)
+    new_customers = pd.get_dummies(new_customers, drop_first=True)
+    new_customers["Sroke_prediction"] = model.predict(new_customers)
+    st.success(f"You successfully scored %i new customers for stroke predictions" % new_customers.shape)
 
 
-#@st.cache(allow_output_mutation=True)
-#def load_model():
-#    filename = "stroke_model_capstone"
-#    loaded_model = pickle.load(open(filename, "rb"))
-#    return loaded_model
-# model = load_model()
-
-#new_customers = pd.get_dummies(new_customers, drop_first = True)
-#new_customers["Sroke_prediction"] = model.predict(new_customers)
-
-#st.success(f"You successfully scored %i new customers for stroke predictions" % new_customers.shape)
-
-#st.download_button(label = "Download scored customer data",
-#                   data = new_customers.to_csv(index=False).encode("utf-8"),
-#                   file_name = "scored:customer_data.csv")
+st.download_button(label = "Download scored customer data",
+                   data = new_customers.to_csv(index=False).encode("utf-8"),
+                   file_name = "scored:customer_data.csv")
